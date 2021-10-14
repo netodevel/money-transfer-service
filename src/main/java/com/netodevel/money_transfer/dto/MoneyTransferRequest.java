429513033dadd2bc33159a1918d5ec7a14015285
NetoDevel <josevieira.dev@gmail.com>
2021-10-14 18:04:08 -0300

    Fixs native mode with records

--- src/main/java/com/netodevel/money_transfer/dto/MoneyTransferRequest.java
+++ src/main/java/com/netodevel/money_transfer/dto/MoneyTransferRequest.java
@@ -3,9 +3,10 @@ package com.netodevel.money_transfer.dto;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @JsonSerialize
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record MoneyTransferRequest(String fromAccount, String toAccount, BigDecimal amount) {
+public record MoneyTransferRequest(String fromAccount, String toAccount, BigDecimal amount) implements Serializable {
 }
